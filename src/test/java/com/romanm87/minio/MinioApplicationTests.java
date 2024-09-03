7289b0b9db4313b1f16359b8c8a3ca814e425a05
Roman Matveev <r.matveev@socrat.marketing>
2024-09-03 12:55:47 +1000

    03.09.2024 -- Первичный комит проекта.

---
+++ src/test/java/com/romanm87/minio/MinioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.romanm87.minio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
