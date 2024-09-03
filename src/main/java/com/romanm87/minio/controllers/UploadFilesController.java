ebef9e86d6db5ae5e375def59c7d3a5f1fffd6d9
Roman Matveev <r.matveev@socrat.marketing>
2024-09-03 14:21:39 +1000

    03.09.2024 -- Класс UploadFilesController.java: связка логики FileService.java с методами uploadFile() и getFile()

--- src/main/java/com/romanm87/minio/controllers/UploadFilesController.java
+++ src/main/java/com/romanm87/minio/controllers/UploadFilesController.java
@@ -1,9 +1,15 @@
 package com.romanm87.minio.controllers;
 
+import com.romanm87.minio.dto.FileResource;
 import com.romanm87.minio.services.FileService;
+import io.minio.errors.*;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
 
 @RestController
 @RequestMapping(value = "/api/file")
@@ -15,12 +21,12 @@ public class UploadFilesController {
     }
 
     @PostMapping(value = "/upload")
-    public ResponseEntity<?> uploadFile(@RequestParam("file") MultipartFile file, @RequestParam("bucket") String bucket) {
-        return ResponseEntity.ok().build();
+    public ResponseEntity<FileResource> uploadFile(@RequestParam("file") MultipartFile file, @RequestParam("bucket") String bucket) throws IOException, InvalidResponseException, InvalidKeyException, NoSuchAlgorithmException, ServerException, ErrorResponseException, XmlParserException, InsufficientDataException, InternalException {
+        return ResponseEntity.ok(this.fileService.singleUpload(file, bucket));
     }
 
     @GetMapping(value = "/download/{bucket}/{fileName}")
-    public ResponseEntity<?> getFile(@PathVariable("bucket") String bucket, @PathVariable("fileName") String fileName) {
-        return ResponseEntity.ok().build();
+    public ResponseEntity<byte[]> getFile(@PathVariable("bucket") String bucket, @PathVariable("fileName") String fileName) throws IOException, InvalidResponseException, InvalidKeyException, NoSuchAlgorithmException, ServerException, ErrorResponseException, XmlParserException, InsufficientDataException, InternalException {
+        return ResponseEntity.ok(this.fileService.singleDownload(bucket, fileName));
     }
 }
