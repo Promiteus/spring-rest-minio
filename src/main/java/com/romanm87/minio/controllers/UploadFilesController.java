45316f00691058f5c56a5d3947142c751faa79fe
Roman Matveev <r.matveev@socrat.marketing>
2024-09-03 13:54:28 +1000

    03.09.2024 -- Класс контроллера UploadFilesController.java: методам uploadFile() и getFile() обозначены параметры.

--- src/main/java/com/romanm87/minio/controllers/UploadFilesController.java
+++ src/main/java/com/romanm87/minio/controllers/UploadFilesController.java
@@ -1,22 +1,20 @@
 package com.romanm87.minio.controllers;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping(value = "/api/file")
 public class UploadFilesController {
 
     @PostMapping(value = "/upload")
-    public ResponseEntity<?> uploadFile() {
+    public ResponseEntity<?> uploadFile(@RequestParam("file") MultipartFile file, @RequestParam("bucket") String bucket) {
         return ResponseEntity.ok().build();
     }
 
-    @GetMapping(value = "/download")
-    public ResponseEntity<?> getFile() {
+    @GetMapping(value = "/download/{bucket}/{fileName}")
+    public ResponseEntity<?> getFile(@PathVariable("bucket") String bucket, @PathVariable("fileName") String fileName) {
         return ResponseEntity.ok().build();
     }
 }
