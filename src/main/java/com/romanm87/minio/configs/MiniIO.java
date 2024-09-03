4dea612206278f02e58c1f518f1fa0e5fcedbab5
Roman Matveev <r.matveev@socrat.marketing>
2024-09-03 13:07:14 +1000

    03.09.2024 -- Создание конфигурации для minio client ** установлены тестовые ключи безопасности.

--- src/main/java/com/romanm87/minio/configs/MiniIO.java
+++ src/main/java/com/romanm87/minio/configs/MiniIO.java
@@ -8,6 +8,6 @@ import org.springframework.context.annotation.Configuration;
 public class MiniIO {
     @Bean
     public MinioClient minioClient() {
-        return MinioClient.builder().endpoint("http://localhost:9000").credentials("", "").build();
+        return MinioClient.builder().endpoint("http://localhost:9000").credentials("WRlv4AYIKYCGqMfUsCfZ", "wpm4xZuYVXEx8zRuXVv8s8zCvYx1s2enl7uAwjzE").build();
     }
 }
