18debf9cdb88ca877ecee7236aa27c34d433ef09
Roman Matveev <r.matveev@socrat.marketing>
2024-09-05 12:54:12 +1000

    05.09.2024 -- Класс ExceptionController.java: возврат к 500-му коду для unknownException()

--- src/main/java/com/romanm87/minio/controllers/ExceptionController.java
+++ src/main/java/com/romanm87/minio/controllers/ExceptionController.java
@@ -19,7 +19,7 @@ import java.util.stream.Collectors;
 @RestControllerAdvice
 public class ExceptionController {
 
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     @ExceptionHandler(value = { Exception.class })
     public ErrorResponse unknownException(Exception ex, WebRequest req) {
         return ErrorResponse.builder().errorMessage(ex.getMessage()).build();
